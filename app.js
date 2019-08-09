362dd97d4e483b4cb46afc1ecc4f8225d923c578
windyakin <windyakin@gmail.com>
2019-08-09 16:13:00 +0900

    :sparkles: Feature export

---
+++ app.js
@@ -0,0 +1,26 @@
+const { WebClient } = require('@slack/web-api');
+const request = require('request-promise-native');
+const fs = require('fs').promises;
+const del = require('del');
+
+const exportDirName = 'emoji';
+const slack = new WebClient(process.env.SLACK_TOKEN);
+
+(async () => {
+  try {
+    (await fs.stat(exportDirName)).isDirectory();
+    await del(exportDirName);
+  } finally {
+    await fs.mkdir(exportDirName);
+  }
+
+  const list = await slack.emoji.list();
+
+  await Object.entries(list.emoji).forEach(async ([name, url]) => {
+    if (!url.match(/^alias:/)) {
+      const extention = url.match(/\.[^\.]+$/);
+      const response = await request.get(url, { encoding: null });
+      await fs.writeFile(`${exportDirName}/${name}${extention}`, response);
+    }
+  });
+})();
